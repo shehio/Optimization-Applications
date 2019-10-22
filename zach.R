26143bf8f6167a5986be0bf08007112f6ae044cf
shehio <shehabyasser@gmail.com>
2019-10-22 06:22:00 -0700

    Solved the optimization problem for yolanda too.

--- zach.R
+++ zach.R
@@ -40,7 +40,7 @@ solveMIP = function(mat, rlower, rupper, clower, cupper, obj, types, fname1, fna
   
   # solve LP problem using Simplex Method for relaxation
   solveSimplexGLPK(lp)
-  getObjValGLPK(lp)
+  print(getObjValGLPK(lp))
   
   # # value of variables in optimal solution
   # getColsPrimGLPK(lp)
@@ -66,14 +66,23 @@ solveMIP = function(mat, rlower, rupper, clower, cupper, obj, types, fname1, fna
   # getRowsDualGLPK(lp)
 }
 
-prepMIP = function(mat, pChar1Target, pChar3Target, sCharTarget, target, countTarget, fname1, fnam2)
+prepMIP = function(mat, pChar1Target, pChar3Target, sCharTarget, target, countTarget, fname1, fname2, isZach)
 {
-  nrows = 44
-  ncols = 70
+  nrows = dim(mat)[1]
+  ncols = dim(mat)[2]
+  
+  if (isZach)
+  {
+    allocations = 5
+  }
+  else
+  {
+    allocations = 7
+  }
   
   ## row upper and lower bounds
-  rlower = c(1, pChar1Target, pChar3Target, sCharTarget, target, rep(-Inf, 15), -Inf, rep(1, 5))
-  rupper = c(1, pChar1Target, pChar3Target, sCharTarget, target, rep(0, 15), count, rep(1, 5))
+  rlower = c(1, pChar1Target, pChar3Target, sCharTarget, target, rep(-Inf, 15), -Inf, rep(1, allocations))
+  rupper = c(1, pChar1Target, pChar3Target, sCharTarget, target, rep(0, 15), countTarget, rep(1, allocations))
   
   ## column upper and lower bounds
   clower = rep(0, ncols)
@@ -90,7 +99,7 @@ prepMIP = function(mat, pChar1Target, pChar3Target, sCharTarget, target, countTa
 eye5 = diag(1, 5)
 eye15 = diag(1, 15)
 
-# Zach:
+# Zach and Yolanda:
 # primary constraints
 e = rep(1, 15)
 pChar1 = c(rep(1, 12), rep(0, 3))
@@ -104,17 +113,25 @@ sChar4 = c(rep(0, 10), rep(1, 2), rep(0, 3))
 sChar5 = c(rep(0, 13), 1, 0)
 
 ## Add ds and deltas to the model
-pChars = cbind(rbind(e, pChar1, pChar3), , diag(0, 3, 55))
+pChars = cbind(rbind(e, pChar1, pChar3), diag(0, 3, 55))
 sChars = cbind(rbind(sChar1, sChar2, sChar3, sChar4, sChar5), eye5, -eye5, diag(0, 5, 15), diag(0, 5, 30))
 equalities = cbind(eye15, diag(0, 15, 10), eye15, -eye15, diag(0, 15, 15))
 binaries = cbind(eye15, diag(0, 15, 40), -eye15)
 count = c(rep(0, 55), rep(1, 15))
-already_invested = rbind(c(rep(0, 1, 55), 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+already_invested_zach = rbind(c(rep(0, 1, 55), 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
                          c(rep(0, 1, 55), 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
                          c(rep(0, 1, 55), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0),
                          c(rep(0, 1, 55), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
                          c(rep(0, 1, 55), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1))
-mat = rbind(pChars, sChars, equalities, binaries, count, already_invested) # 3 + 5 + 15  + 15 + 1 + 5
+already_invested_yolanda = rbind(c(rep(0, 1, 55), 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+                              c(rep(0, 1, 55), 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+                              c(rep(0, 1, 55), 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0),
+                              c(rep(0, 1, 55), 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0),
+                              c(rep(0, 1, 55), 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0),
+                              c(rep(0, 1, 55), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0),
+                              c(rep(0, 1, 55), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0))
+mat_zach = rbind(pChars, sChars, equalities, binaries, count, already_invested_zach) # 3 + 5 + 15  + 15 + 1 + 5
+mat_yolanda = rbind(pChars, sChars, equalities, binaries, count, already_invested_yolanda) # 3 + 5 + 15  + 15 + 1 + 5
 
 # Model 1:
 target = c(0.3, 0.09, 0.07, 0.2, 0.08, 0.06, 0.03, 0.03, 0.02, 0.02, 0.01, 0.02, 0.07, 0.0, 0.0)
@@ -124,4 +141,54 @@ sCharTarget = c(0.511, 0.106, 0.07, 0.03, 0.00)
 countTarget = 6
 fname1 = "solution-1-M1-Zach.txt"
 fname2 = "solution-2-M1-Zach.txt"
-prepMIP(mat, pChar1Target, pChar3Target, sCharTarget, target, countTarget, fname1, fnam2)
+prepMIP(mat_zach, pChar1Target, pChar3Target, sCharTarget, target, countTarget, fname1, fname2, TRUE)
+
+# Model 2:
+target = c(0.24, 0.09, 0.06, 0.11, 0.05, 0.05, 0.03, 0.03, 0.01, 0.01, 0.01, 0.03, 0.26, 0.0, 0.02)
+pChar1Target = 0.72
+pChar3Target = 0.07
+sCharTarget = c(0.4355, 0.095, 0.05, 0.04, 0.26)
+countTarget = 6
+fname1 = "solution-1-M2-Zach.txt"
+fname2 = "solution-2-M2-Zach.txt"
+prepMIP(mat_zach, pChar1Target, pChar3Target, sCharTarget, target, countTarget, fname1, fname2, TRUE)
+
+# Model 3: Minimizes the penalty for Zach.
+target = c(0.16, 0.06, 0.04, 0.08, 0.04, 0.03, 0.02, 0.02, 0.01, 0.01, 0.02, 0.04, 0.35, 0.04, 0.08)
+pChar1Target = 0.53
+pChar3Target = 0.12
+sCharTarget = c(0.2885, 0.063, 0.04, 0.06, 0.04)
+countTarget = 6
+fname1 = "solution-1-M3-Zach.txt"
+fname2 = "solution-2-M3-Zach.txt"
+prepMIP(mat_zach, pChar1Target, pChar3Target, sCharTarget, target, countTarget, fname1, fname2, TRUE)
+
+# Model 1:
+target = c(0.3, 0.09, 0.07, 0.2, 0.08, 0.06, 0.03, 0.03, 0.02, 0.02, 0.01, 0.02, 0.07, 0.0, 0.0)
+pChar1Target = 0.93
+pChar3Target = 0.07
+sCharTarget = c(0.511, 0.106, 0.07, 0.03, 0.00)
+countTarget = 8
+fname1 = "solution-1-M1-Yolanda.txt"
+fname2 = "solution-2-M1-Yolanda.txt"
+prepMIP(mat_yolanda, pChar1Target, pChar3Target, sCharTarget, target, countTarget, fname1, fname2, FALSE)
+
+# Model 2:
+target = c(0.24, 0.09, 0.06, 0.11, 0.05, 0.05, 0.03, 0.03, 0.01, 0.01, 0.01, 0.03, 0.26, 0.0, 0.02)
+pChar1Target = 0.72
+pChar3Target = 0.07
+sCharTarget = c(0.4355, 0.095, 0.05, 0.04, 0.26)
+countTarget = 8
+fname1 = "solution-1-M2-Yolanda.txt"
+fname2 = "solution-2-M2-Yolanda.txt"
+prepMIP(mat_yolanda, pChar1Target, pChar3Target, sCharTarget, target, countTarget, fname1, fname2, FALSE)
+
+# Model 3: Also, minimizes the penalty for Yolanda.
+target = c(0.16, 0.06, 0.04, 0.08, 0.04, 0.03, 0.02, 0.02, 0.01, 0.01, 0.02, 0.04, 0.35, 0.04, 0.08)
+pChar1Target = 0.53
+pChar3Target = 0.12
+sCharTarget = c(0.2885, 0.063, 0.04, 0.06, 0.04)
+countTarget = 8
+fname1 = "solution-1-M3-Yolanda.txt"
+fname2 = "solution-2-M3-Yolanda.txt"
+prepMIP(mat_yolanda, pChar1Target, pChar3Target, sCharTarget, target, countTarget, fname1, fname2, FALSE)
